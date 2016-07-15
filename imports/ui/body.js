35f4d805d48e91a8487ba83c2183053b735e0150
Ivan Tarozzi <itarozzi@gmail.com>
2016-07-15 18:43:48 +0200

    add API script to load data from Mongo DB

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,12 +1,21 @@
 import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks.js';
 import './body.html';
 
 
+// Assign static data
+// Template.body.helpers({
+//   tasks: [
+//     { text: 'This is task 1' },
+//     { text: 'This is task 2' },
+//     { text: 'This is task 3' },
+//   ],
+// });
 
+
+// Load data from Mongo DB
 Template.body.helpers({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
+  tasks() {
+    return Tasks.find({});
+  },
 });
