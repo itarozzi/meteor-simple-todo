349e3f5c8ac919d55408032e9f9a434537471cbb
Ivan Tarozzi <itarozzi@gmail.com>
2016-07-21 17:36:12 +0200

    lint fix

--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -1,9 +1,6 @@
-import { Accounts } from 'meteor/accounts-base';
-
+import { Accounts } from 'meteor/accounts-base'
 
 Accounts.ui.config({
-    // Configure the accounts UI to use usernames intead of email addresses
-    passwordSignupFields: 'USERNAME_ONLY',
-});
-
-
+  // Configure the accounts UI to use usernames intead of email addresses
+  passwordSignupFields: 'USERNAME_ONLY'
+})
